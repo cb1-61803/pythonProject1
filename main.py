0e0228f5ef6e178417813047aaef31ee9c0b71b5
cb1-61803 <76478260+cb1-61803@users.noreply.github.com>
2021-01-16 19:37:52 +0000

    I LOVE DAVE

--- main.py
+++ main.py
@@ -31,6 +31,18 @@ print(type(data))
 # Print the keys of the file
 for key in data.keys():
     print(key)
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
+
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute('SELECT * FROM Employee WHERE EmployeeId >= 6')
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+
+# Print the head of the DataFrame df
+print(df.head())
 
 
 
